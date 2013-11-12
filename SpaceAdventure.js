118a78e912db368573b670eeb13e33e73c375529
KLynch13 <Kevin.Lynch2@marist.edu>
2013-11-12 13:16:27 -0500

    Organized functions, added Explore function

--- SpaceAdventure.js
+++ SpaceAdventure.js
@@ -10,30 +10,38 @@ function getName()
 
 function getTrait(item)
 {
-if(item==="gun")
-{
-gun=true;
-}
-else if(item==="teleporter")
-{
-teleporter=true;
-}
-else
-{
-comm=true;
-}
+ if(item==="gun")
+ {
+  gun=true;
+ }
+ else if(item==="teleporter")
+ {
+  teleporter=true;
+ }
+ else
+ {
+  comm=true;
+ }
 }
 
 
 function getPic(picID)
 {
-pic=picID;
+ pic=picID;
 }
 
 
 //main functions
 function picSwitcher(imgID)
 {
-imgName='images/'+imgID+'.JPG';
-document.getElementById('imgMain').src=imgName;
+ imgName='images/'+imgID+'.JPG';
+ document.getElementById('imgMain').src=imgName;
+}
+
+function Explore(location)
+{
+ if (location==="")
+ {
+  document.getElementById('').innerHTML=
+ } 
 }
