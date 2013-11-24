967aff8a9d85a4b3eb4db8713789b1c1599db016
pmagent2013 <pmagent2013@gmail.com>
2013-11-24 12:54:08 -0500

    Added explore and search functions for the ship
    some code is currently commented out as to not have to read dialogue
    over and over

--- SpaceAdventure.js
+++ SpaceAdventure.js
@@ -1,6 +1,6 @@
 //Character Creation Functions
 
-var name="", gun=false, teleporter=false, comm=false, pic="", mainPic="shipOuter", avatarPic="", advanceTalkCounter=1;
+var name="", gun=false, teleporter=false, comm=false, medicine=false, pic="", mainPic="shipOuter", avatarPic="", advanceTalkCounter=1;
 
 function getName()
 {
@@ -40,10 +40,11 @@ function startGame()
    
 }
 
-function picSwitcher(imgID) //switches main image
+function picSwitcher(imgName) //switches main image
 {
-  
-    document.getElementById('imgMain').innerHTML = "<img src='images/"+imgID+".jpg' height='450px' width='90%'>";
+	
+    document.getElementById('imgMain2').innerHTML = "<img src='images/"+imgName+".jpg' height='450px' width='90%'>";
+	
 }
 
 function avatar(ava) //selects the avatar of the user
@@ -72,6 +73,17 @@ function textDisplay(text) // text that will be spoken
 {
   ai='ai'
   document.getElementById('speakerPic').innerHTML = document.getElementById('speakerPic').innerHTML + text + '<input type="button" value="Next" onClick="advanceTalk();">';
+}
+
+function characterPic2(avatarIMG) //avatar of who is speaking
+{
+  
+  document.getElementById('speakerPic2').innerHTML = "<img src='images/"+avatarIMG+".jpg' height='115px' width='75px'>";
+}
+
+function textDisplay2(text) // text that will be spoken
+{
+    document.getElementById('speakerPic2').innerHTML = document.getElementById('speakerPic2').innerHTML + text + '<input type="button" value="Next" onClick="advanceTalk();">';
 }
 
 function advanceTalk(pic, text) //selects the avatar of who speaking and the dialogue that will be said.
@@ -172,12 +184,119 @@ function advanceTalk(pic, text) //selects the avatar of who speaking and the dia
 		gameShip(); //start game on the ship
 		
 	}
+	else if(advanceTalkCounter===498)
+	{
+		characterPic2(avatarPic);
+		textDisplay2('');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===500)
+	{
+		characterPic2(avatarPic);
+		textDisplay2('There is nothing of interest here');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===502)
+	{
+		characterPic2(avatarPic);
+		textDisplay2('There is a computer in the center of the room, it\'s files may be accessed.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===504)
+	{
+		characterPic2('ai');
+		textDisplay2('Most of this computers files have been damaged the only ones i can access for you are about the ships journey');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===505)
+	{
+		characterPic2('ai');
+		textDisplay2('This ship was launched from earth 300 years ago with the goal of discovering inhabitable worlds outside our galaxy. This ship was one of many launched.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===506)
+	{
+		characterPic2('ai');
+		textDisplay2('Each ship carried a crew of 500, Most were kept in stasis to carry out the mission when those awake passed away.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===507)
+	{
+		characterPic2('ai');
+		textDisplay2('For over 200 years the ships explored the galaxies around the Milky Way, by this time the crews of many ships were down to only 50 crew members in stasis.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===508)
+	{
+		characterPic2('ai');
+		textDisplay2('At this time we began to lose contact with the ships furthest out. For the next 50 years more and more ships dropped off the grid with no explanation.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===509)
+	{
+		characterPic2('ai');
+		textDisplay2('Finally a ship got off a transmission before it dropped off, it warned of a great enemy whose technology far outpowered our own, and they seemed bent on total destruction.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===510)
+	{
+		characterPic2('ai');
+		textDisplay2('Too far out to warn earth, our final five ships formed a convoy and headed toward earth.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===511)
+	{
+		characterPic2('ai');
+		textDisplay2('On the edge of the Pegasus Galaxy, they caught up to us. Two giant warships bearing down, we gave it all we could, we managed to destroy 1 but 4 of our ships were destroyed. In a desperate measure the remaining crew of this ship boarded fighters and commited a suicide run aganist the enemy');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===512)
+	{
+		characterPic2('ai');
+		textDisplay2('This ship was heavily damaged but they managed to destroy the enemy ship. You are the only crew member remaining, and without our long range communications you must get back to earth to warn them.');
+		advanceTalkCounter++;
+		
+		
+	}
+	else if(advanceTalkCounter===514)
+	{
+		characterPic2(avatarPic);
+		textDisplay2('There appears to be a crate of medicine in the corner. Perhaps this could be used as a trade with some locals.');
+		advanceTalkCounter++;
+		
+		
+	}
+	
 }
 
 function gameShip()
 {
 	gameBegin.style.display='none'
 	game.style.display='block'
+	characterPic2(avatarPic);
+	
 	
 }
 
@@ -185,8 +304,49 @@ function explore(location)
 {
  if (location==="stasisChamber")
  {
-    buttonStart.style.display='none'
-    buttonExplore.style.display='block'
+    buttonStart_Stasis.style.display='none'
+	buttonStart_ControlRoom.style.display='none'
+	buttonStart_EmbarkationRoom.style.display='none'
+    buttonExplore_Stasis.style.display='block'
  } 
+ if (location==="embarkationRoom")
+ {
+    buttonStart_Stasis.style.display='none'
+	buttonStart_ControlRoom.style.display='none'
+	buttonStart_EmbarkationRoom.style.display='none'
+    buttonExplore_EmbarkationRoom.style.display='block'
+ }
+ if (location==="controlRoom")
+ {
+    buttonStart_Stasis.style.display='none'
+	buttonStart_ControlRoom.style.display='none'
+	buttonStart_EmbarkationRoom.style.display='none'
+    buttonExplore_ControlRoom.style.display='block'
+ }
+}
+
+function search(location)
+{
+ if (location==="stasisChamber")
+ {
+    advanceTalkCounter=500;
+    advanceTalk();
+	
+ } 
+ if (location==="embarkationRoom")
+ {
+	buttonStart_EmbarkationRoom.style.display='none'
+    advanceTalkCounter=514;
+    advanceTalk();
+	medicine=true;
+	buttonSearch_EmbarkationRoom.style.display='block'
+ }
+ if (location==="controlRoom")
+ {
+	buttonStart_ControlRoom.style.display='none'
+	advanceTalkCounter=502;
+    advanceTalk();
+    buttonSearch_ControlRoom.style.display='block'
+ }
 }
 
